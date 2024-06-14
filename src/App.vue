bda58846ea31cde7e9a985ed810d3a2647c9d783
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-06-14 15:44:27 +0200

    problema

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script>
+  import AppMain from './components/AppMain.vue';
 
   export default {
     data(){
       return{
         
       }
+    },
+    components:{
+      AppMain,
     }
   }
 </script>
 
 <template>
-
+  <AppMain/>
 </template>
 
 <style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,46 @@
 <script>
+  import axios from 'axios';
 
   export default {
     data(){
       return{
-        
+        inputText:'',
       }
+    },
+    methods:{
+      getFilms(){
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=4abd14a4bda72bd354eacef4ac4da057&query=$ {inputText}')
+
+        .then((response) => {
+          // handle success
+          console.log(response);
+          
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function () {
+          // always executed
+        });
+      }
+    },
+    created(){
+      // this.getFilms();
     }
   }
 </script>
 
 <template>
-<input type="text" name="" id="">
-<button></button>
+  <main>
+    <input type="text" name="" id="search-input" v-model="inputText">
+    <button @click="getFilms()">search</button>
+  </main>
 </template>
 
 <style scoped>
 
 </style>
+
+
+
