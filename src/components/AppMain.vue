afa8443c7661fb052f9ca825f53a66e50b800632
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-06-14 11:57:40 +0200

    scaffolding

---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+
+  export default {
+    data(){
+      return{
+        
+      }
+    }
+  }
+</script>
+
+<template>
+<input type="text" name="" id="">
+<button></button>
+</template>
+
+<style scoped>
+
+</style>
